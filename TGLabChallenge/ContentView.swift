8970aab2970d82412e30526aa58fc8ae2dc64b65
Sergio Cesar <sergiocesar0203@gmail.com>
2025-10-19 17:36:36 -0400

    feat(teamsView): implement teams List -WIP

--- TGLabChallenge/ContentView.swift
+++ TGLabChallenge/ContentView.swift
@@ -20,7 +20,6 @@ struct ContentView: View {
                 .tabItem {
                     Label("Jogos", systemImage: "gamecontroller")
                 }
-
         }
     }
 }
--- TGLabChallenge/Presenter/Views/TeamsView.swift
+++ TGLabChallenge/Presenter/Views/TeamsView.swift
@@ -6,17 +6,47 @@
 //
 
 import SwiftUI
-
+import NBAService
 struct TeamsView: View {
     @Environment(NBAViewModel.self) var vm
     var body: some View {
-        
+        NavigationStack {
+            ScrollView {
+                Grid(alignment: .leading) {
+                    GridRow {
+                        Text("Name")
+                        Text("City")
+                        Text("Conference")
+                    }
+                    .bold()
+
+                    Divider()
+
+                    ForEach(vm.teamsList) { team in
+                        GridRow(alignment: .center){
+                            Text(team.fullName)
+                            Text(team.city)
+                            Text(team.conference)
+                            Image(systemName: "chevron.right")
+                        }
+                        Divider()
+                    }
+                }
+                .padding()
+            }
+            .padding(.horizontal, 8)
+            .navigationTitle("Teams")
+            .onAppear() {
+                vm.fetchTeams()
+            }
+        }
     }
 }
 
 #if DEBUG
 #Preview {
+    @State var vm: NBAViewModel = .init()
     TeamsView()
-        .environment(NBAViewModel())
+        .environment(vm)
 }
 #endif
